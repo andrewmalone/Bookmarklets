bf0682200e9eaffc731988e68e87b0a806544c5d
Andrew <andrew@andrewmalone.com>
2011-08-30 15:00:17 -0400

    removed jquery dependency

--- cleanJIRAcomment.js
+++ cleanJIRAcomment.js
@@ -1,29 +1,13 @@
-if (typeof jQuery == 'undefined') {
-	loadScript('https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js',runthis);
-} else {
-	runthis();
+var t = document.getElementById("comment").value;
+t = t.replace(/=\n/gi,"");
+t = t.replace(/=20/g,"");
+var n = new Array("=C2=B7","=E2=80=99","=C2=A9","=E2=80=98","=E2=80=93");
+for (var i=0; i<n.length; i++)
+{
+	t = r(t,n[i]);
 }
-
-function loadScript(script,onLoad) {
-	var s = document.createElement('script');
-	s.type = 'text/javascript';
-	s.onload = onLoad;
-	s.src = script;
-	document.body.appendChild(s);
-}
-
-function runthis() {
-	var t = $("#comment").text();
-	t = t.replace(/=\n/gi,"");
-	t = t.replace(/=20/g,"");
-	var n = new Array("=C2=B7","=E2=80=99","=C2=A9","=E2=80=98","=E2=80=93");
-	for (var i=0; i<n.length; i++)
-	{
-		t = r(t,n[i]);
-	}
-	t = t.replace(/\n\n/g,'\n');
-	$("#comment").text(t);
-}
+t = t.replace(/\n\n/g,'\n');
+document.getElementById("comment").value=t;
 
 function r(str,sub)
 {
