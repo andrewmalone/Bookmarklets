31f88a1bc75d13891b8de101bf2892b9c56f0bbe
Andrew Malone <andrew@andrewmalone.com>
2012-07-10 17:03:31 -0400

    fixing segment

--- gotoSegmentId.js
+++ gotoSegmentId.js
@@ -51,7 +51,7 @@ function runthis() {
 		buttons: {
 			"OK": function() {
 				// NEEDS INPUT VALIDATION!!
-				var urlstring = "/gmas/dispatch?formName=SegmentHomeForm&ProjectListSegmentHomeEvent=&segmentId=";
+				var urlstring = "/gmas/project/SCR0104SegmentHome.jsp?segmentId=";
 				$(this).dialog("close");
 				window.location = $("#env").val() + urlstring + $.trim($("#segmentId").val());			
 			}
