bcfd4b1547a5b9080fc8b8b8c88311a5c397f76f
andrewmalone <andrew@andrewmalone.com>
2014-03-14 10:17:26 -0400

    Trying again to fix paste detection on Mac

--- gotoSegmentId.js
+++ gotoSegmentId.js
@@ -86,8 +86,7 @@ $("#env").value = 'https://' + window.location.host;
 $("#segmentId").addEventListener("keyup", up, false);
 
 function up(e) {
-	console.log(e)
-	if ((e.metaKey || e.ctrlKey) && e.keyCode == 86) {
+	if ((e.keyIdentifier == "Meta" || e.ctrlKey) && e.keyCode == 86) {
 		var urlstring = "/gmas/project/SCR0104SegmentHome.jsp?segmentId=";
 		//$(this).dialog("close");
 		window.location = $("#env").value + urlstring + this.value.trim();
